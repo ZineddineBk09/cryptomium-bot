4dd139c4baa3dc263c37a76d40fd86190d7c30e3
ZineddineBk09 <eino1209@gmail.com>
2023-09-24 14:10:30 +0100

    functions

---
+++ utils/index.ts
@@ -0,0 +1,91 @@
+import axios from "axios"
+
+export async function getLatestNews(url: string = 'http://localhost:3000/api/news') {
+  try {
+    const response = await axios.get(url)
+    const data = response.data
+
+    return data
+  } catch (error) {
+    console.log('getLatestNews Error:', error)
+    return []
+  }
+}
+
+export async function getLatestNewsByCategory(category: string) {
+  try {
+    const response = await axios.get(
+      'http://localhost:3000/api/news?category=' + category.toUpperCase()
+    )
+    const data = response.data
+
+    return data
+  } catch (error) {
+    console.log('getLatestNewsByCategory Error:', error)
+    return []
+  }
+}
+
+export async function getCategories() {
+  try {
+    const data = await getLatestNews()
+    let categories: string[] = []
+    // get unique categories from the data
+    for (let i = 0; i < data.length; i++) {
+      if (!categories.includes(data[i].category)) {
+        categories.push(data[i].category)
+      }
+    }
+    return categories
+  } catch (error) {
+    console.log('getCategories Error:', error)
+    return []
+  }
+}
+
+export async function getSecurityNewsCategories() {
+  try {
+    const data = await getLatestNews('http://localhost:3000/api/security-news')
+    let categories: string[] = []
+    // get unique categories from the data
+    for (let i = 0; i < data.length; i++) {
+      if (!categories.includes(data[i].category)) {
+        categories.push(data[i].category)
+      }
+    }
+    return categories
+  } catch (error) {
+    console.log('getCategories Error:', error)
+    return []
+  }
+}
+
+export async function getLatestCryptoPrices(currency: string = 'usd') {
+  try {
+    const URL =
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=' + currency
+    const response = await axios.get(URL)
+    const data = response.data
+
+    return data
+  } catch (error) {
+    console.log('getLatestCryptoPrices Error:', error)
+    return []
+  }
+}
+
+export async function getCurrencyInfos(currency: string) {
+  try {
+    const URL =
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=' +
+      currency
+    const response = await axios.get(URL)
+    const data = response.data
+
+    return data
+  } catch (error) {
+    console.log('getCurrencyInfos Error:', error)
+    return []
+  }
+}
+
