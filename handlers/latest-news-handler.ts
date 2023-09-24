2aec8776ba00d3697c33cb37e5fdce769091719b
ZineddineBk09 <eino1209@gmail.com>
2023-09-24 14:10:25 +0100

    handler

---
+++ handlers/latest-news-handler.ts
@@ -0,0 +1,22 @@
+import { Bot, Context, InlineKeyboard } from 'grammy'
+import axios from 'axios'
+import 'dotenv/config'
+import { getCategories } from '../utils'
+
+export const newsCategoryMenu = getCategories().then((categories) => {
+  const menu = new InlineKeyboard()
+  for (let i = 0; i < categories.length; i++) {
+    menu.text(
+      categories[i],
+      'latest_news_by_category_' + categories[i].toLocaleLowerCase()
+    )
+  }
+  return menu.row().text('Back to Main Menu', 'back_to_main_menu')
+})
+
+export async function handleWelcome(ctx: Context) {
+  console.log('latest_news')
+  await ctx.reply('Latest News on Cointelegraph', {
+    reply_markup: await newsCategoryMenu,
+  })
+}
