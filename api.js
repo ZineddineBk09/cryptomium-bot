46b10072e7b7f1c7e3cc270daae4226d4fe34be9
ZineddineBk09 <eino1209@gmail.com>
2023-09-25 08:49:30 +0100

    fix

--- api.js
+++ api.js
@@ -3,6 +3,8 @@ const server = jsonServer.create()
 const router = jsonServer.router('./db.json')
 const middlewares = jsonServer.defaults()
 const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
 
 // Use default middlewares
 server.use(middlewares)
@@ -19,8 +21,11 @@ server.listen(PORT, () => {
 
 // to prevent the server from sleeping every 15 minutes when deployed on render.com
 // we will ping the server every 5 minutes
-const http = require('http')
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000'
 setInterval(() => {
-  console.log('Pinging server...')
-  http.get('https://cryptomium-bot.onrender.com')
+  console.log('Pinging server...', SERVER_URL)
+  fetch(SERVER_URL)
+    .then((res) => res.text())
+    .then((body) => console.log(body))
+    .catch((err) => console.error(err))
 }, 300000) // every 5 minutes (300000)
